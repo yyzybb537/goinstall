b6fed421c19915fae80a2e688037523b6c9b5d32
tedcy <yue.cheng.ted@gmail.com>
2018-02-06 17:25:23 +0800

    扫描软连接并且忽略报错

--- goinstall.go
+++ goinstall.go
@@ -7,37 +7,51 @@ import (
 	"strings"
 )
 
-func main() {
-	installed := make(map[string]bool)
 
-	err := filepath.Walk(".", func(path string, f os.FileInfo, err error) error {
+func walk(dirpath string, ifSymLink bool) {
+	installed := make(map[string]bool)
+	filepath.Walk(dirpath, func(path string, f os.FileInfo, err error) error {
 		if f == nil {
-			return err
+			return nil
 		}
 
 		if f.IsDir() {
 			return nil
-        }
+		}
+
+		if f.Mode()&os.ModeSymlink != 0 {
+			symlinkPath, err := filepath.EvalSymlinks(f.Name())
+			if err != nil {
+				println("Error:", err.Error())
+			}
+			walk(symlinkPath, true)
+			return nil
+		}
 
 		if strings.HasSuffix(path, ".go") {
+			if ifSymLink {
+				path = strings.TrimPrefix(path, dirpath)
+				path = filepath.Join(filepath.Base(dirpath), path)
+			}
 			dir := filepath.Dir(path)
 			if installed[dir] {
 				return nil
-            }
-
+			}
 			installed[dir] = true
 			println("Process: go install", dir)
 			cmd := exec.Command("go", "install")
 			cmd.Dir = dir
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
-			err = cmd.Run()
-        }
-
-		return err
+			err := cmd.Run()
+			if err != nil {
+				println("Error:", err.Error())
+			}
+		}
+		return nil
 	})
+}
 
-	if err != nil {
-		println("Error:", err.Error())
-	}
+func main() {
+	walk(".", false)
 }
